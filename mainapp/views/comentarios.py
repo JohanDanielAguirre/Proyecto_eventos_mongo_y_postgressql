b78259458c68e5a674c61d5f50b372b5120c5fc1
alejoamu <alejoamu2@gmail.com>
2024-06-03 14:44:21 -0500

    feat: add the event form

--- mainapp/views/comentarios.py
+++
@@ -1,8 +0,0 @@
-
-from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
-from django.shortcuts import render, redirect
-
-def crearComentario(request):
-    
-    return render(request,"comentarios.html")
