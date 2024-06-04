69c4f72bfe9e8b96e6ce5af58978576d66021c85
Johan Daniel Aguirre Arias <aguirreariasjohandaniel@gmail.com>
2024-06-04 08:42:37 -0500

    fix: some changes in the application neded to work properly

--- mainapp/views/__init__.py
+++ mainapp/views/__init__.py
@@ -2,5 +2,5 @@ from .views_home import *
 from .evento import *
 from .usuarios import *
 from .viewsEventoInfo import *
-from .lugarEventos import *
+from .LugarEventos import *
 from .viewsComment import *
